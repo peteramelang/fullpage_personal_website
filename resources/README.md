28ad54187f559b2ab4abe162eb4645fecb671ce5
pa <pa@s2intermedia.de>
2015-05-15 18:24:38 +0200

    Init

---
+++ resources/README.md
@@ -0,0 +1,6 @@
+## Grunt
+Task [grunt-image](https://www.npmjs.org/package/grunt-image) only supports PNGs.
+
+#### Compass retina & retina-sprites mixin
+* For sprites: @include retina-sprite-background(filename-without-extension);
+* For single images: @include retina-background(filename-without-extension);
---
+++ source/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+	<title>Peter Amelang</title>
+	<meta name="description" content="Beschreibung" />
+	<meta name="keywords" content="Suchbegriffe" />
+	<meta name="robots" content="INDEX, FOLLOW" />
+	<meta name="author" content="Peter Amelang" />
+
+	<link rel="stylesheet" href="css/styles.css" />
+
+	<meta property="og:title" content="FB Titel"/>
+	<meta property="og:image" content="FB Bild"/>
+	<meta property="og:site_name" content="Peter Amelang"/>
+	<meta property="og:description" content="FB Beschreibung"/>
+	<meta property="og:type" content="website" />
+
+	<!--<meta name="msapplication-square70x70logo" content="images/windows-tile-70x70.png">
+	<meta name="msapplication-square150x150logo" content="images/windows-tile-150x150.png">
+	<meta name="msapplication-square310x310logo" content="images/windows-tile-310x310.png">
+	<meta name="msapplication-TileImage" content="images/windows-tile-144x144.png">
+	<meta name="msapplication-TileColor" content="#292929">
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="images/apple-touch-icon-152x152-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="images/apple-touch-icon-120x120-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="images/apple-touch-icon-76x76-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="images/apple-touch-icon-60x60-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/apple-touch-icon-144x144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="images/apple-touch-icon.png">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-title" content="Format D">
+	<link rel="icon" sizes="196x196" href="images/homescreen-196x196.png">
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="icon" type="image/png" sizes="64x64" href="images/favicon.png">-->
+</head>
+<body>
+	<div class="wrapper">
+		<div class="container">
+			<p class="frontpage-text">
+				<strong>Lorem Impsum.</strong>
+				Bl DFDUFD fsu sdfu sdfisisheuih <br>
+				feusdhfosfh  efuebf efje jefjei
+			</p>
+			<div class="logo"></div>
+		</div>
+	</div>
+</body>
+
+
+</html>
